4d4f26f79fd173b24acb018d6bc03ac788e91683
peltevis <agusayerza@gmail.com>
2017-10-12 10:03:24 -0300

    Solucionado bug de validacion de opciones en Menu

--- API/src/UI/Menu.java
+++ API/src/UI/Menu.java
@@ -31,28 +31,23 @@ public class Menu {
         return output + "\n";
     }
 
-    private boolean esOpcionInvalida (int i) throws MenuInvalidOptionSelectedException{
-        if(i < (opcionesMenu.length + 1) && (i >= 0)){
+    private void esOpcionInvalida (int i) throws MenuInvalidOptionSelectedException{
+        if(i > opcionesMenu.length || i <= 0){
             throw new MenuInvalidOptionSelectedException("Se selecciono una opcion invalida");
-        } else {
-            return true;
         }
     }
 
     public int pedirOpcionAlUsuario(){
-        boolean esperarOpcion = true;
         int i = 0;
-        while(esperarOpcion) {
             i = scanner.getInt("Seleccione una opcion: ");
             try{
                 this.esOpcionInvalida(i);
-                esperarOpcion = false;
             }
             catch (MenuInvalidOptionSelectedException e){
                 System.out.println(e.getMessage());
                 System.out.println(this.strPrintMenu());
+                return this.pedirOpcionAlUsuario();    //Viva la recursividad
             }
-        }
 
         return i;
     }
