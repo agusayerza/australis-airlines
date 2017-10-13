9cebf0f15adcb78afff04557fb03a090012b0c58
BoliBerrys <kiper30@gmail.com>
2017-10-13 10:10:52 -0300

    Update Pasajero.java

--- API/src/personas/Pasajero.java
+++ API/src/personas/Pasajero.java
@@ -1,8 +1,20 @@
 package personas;
 
+import avion.Asiento;
+
+import java.util.ArrayList;
+
 public class Pasajero extends Persona{
 
-    public Pasajero() {
+    ArrayList<Asiento> asientosAdquiridos = new ArrayList<>();
+    
+    public Pasajero(ArrayList<Asiento> asientos) {
+        this.asientosAdquiridos = asientos;
+    }
+    
+    public int getCantidadAsientos()
+    {
+        return asientosAdquiridos.size();
     }
 
     //public ... comprarPasaje(Pasaje){
