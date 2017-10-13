dcb428610570e3ab8f88e90fe920d4862babf31f
peltevis <agusayerza@gmail.com>
2017-10-13 09:08:18 -0300

    Cambios

--- App Empleados/src/Consola.java
+++ App Empleados/src/Consola.java
@@ -1,15 +1,13 @@
 import UI.*;
 import vuelo.*;
 
-/**
- * ESTA APLICACION ESTA ACA SOLO PARA PRUEBAS.
- */
+
 public class Consola {
     Menu menuPrincipal;
 
     public static void main(String[] args){
         String[] opcionesMenuPrincipal = new String[9];
         System.out.println("Bienvenido a la aplicacion de Empleados de Australis Airlines");
-        //TODO: Toda esta aplicacion ajajaj -A
+
     }
 }
