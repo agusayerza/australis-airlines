d697ee037fc2eccb9decb1fb4e85d59445ab163b
BoliBerrys <kiper30@gmail.com>
2017-10-28 02:18:58 -0300

    Create MockServer.java

---
+++ API/src/MockServer.java
@@ -0,0 +1,17 @@
+import avion.Asiento;
+import vuelo.Vuelo;
+
+public class MockServer implements Servicios {
+
+    //TODO: Terminar esto
+    public void BuscarVuelo(Vuelo vuelo)
+    {
+
+    }
+
+    public void OcuparAsiento(Asiento asiento)
+    {
+
+    }
+
+}
