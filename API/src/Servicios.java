cd7e26b543d13a1cfe2d8b7b239cfbec661f20c1
BoliBerrys <kiper30@gmail.com>
2017-10-28 02:32:12 -0300

    Update Servicios.java

--- API/src/Servicios.java
+++ API/src/Servicios.java
@@ -1,9 +1,12 @@
 import avion.Asiento;
+import personas.Pasajero;
 import vuelo.Vuelo;
+
+import java.time.LocalDate;
 
 public interface Servicios {
 
     void BuscarVuelo(Vuelo vuelo);
 
-    void OcuparAsiento(Asiento asiento);
+    void OcuparAsiento(LocalDate fecha, String codigoVuelo, String codigoAsiento, Pasajero pasajero);
 }
