e1eefb713cd68ed5148273a23371d2d4ea471963
BoliBerrys <kiper30@gmail.com>
2017-11-07 13:04:50 -0300

    Create Servicios.java

---
+++ App Empleados/src/Servicios.java
@@ -0,0 +1,19 @@
+import avion.Asiento;
+import personas.Pasajero;
+import personas.Piloto;
+import vuelo.Vuelo;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+
+public interface Servicios {
+    //Despues se ajusta
+    ArrayList<Vuelo> getFlightsOnDateFromToDestination(LocalDate date, String aeropuertoDePartida, String aeropuertoDeArribo);
+
+    void venderAsiento(LocalDate date, String codigoDeVuelo, String codigoDeAsiento, Pasajero pasajero);
+
+    ArrayList<Vuelo> BuscarVuelosPiloto(int dni);
+}
+
